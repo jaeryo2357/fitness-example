392072c75cdca1686ee0d61a587b782b5cafa034
Jaeryo <114555778+cleaninglab-jaeryo@users.noreply.github.com>
2025-05-11 03:05:26 +0900

    extract method step5: includeInherited 함수 추출

--- src/main/java/function/FitnessExample.java
+++ src/main/java/function/FitnessExample.java
@@ -27,39 +27,34 @@ public class FitnessExample {
                 if (includeSuiteSetup) {
                     String pageName = SuiteResponder.SUITE_SETUP_NAME;
                     String setup = "!include -setup .";
-                    WikiPage suiteSetup = PageCrawlerImpl.getInheritedPage(pageName, wikiPage);
-                    if (suiteSetup != null) {
-                        includePage(suiteSetup, setup);
-                    }
+                    includeInherited(pageName, setup);
                 }
                 String pageName2 = "SetUp";
                 String setup2 = "!include -setup .";
-                WikiPage setup = PageCrawlerImpl.getInheritedPage(pageName2, wikiPage);
-                if (setup != null) {
-                    includePage(setup, setup2);
-                }
+                includeInherited(pageName2, setup2);
             }
 
             buffer.append(pageData.getContent());
             if (pageData.hasAttribute("Test")) {
                 String pageName3 = "TearDown";
                 String teardown1 = "!include -teardown .";
-                WikiPage teardown = PageCrawlerImpl.getInheritedPage(pageName3, wikiPage);
-                if (teardown != null) {
-                    includePage(teardown, teardown1);
-                }
+                includeInherited(pageName3, teardown1);
                 if (includeSuiteSetup) {
                     String pageName4 = SuiteResponder.SUITE_TEARDOWN_NAME;
                     String teardown2 = "!include -teardown .";
-                    WikiPage suiteTeardown = PageCrawlerImpl.getInheritedPage(pageName4, wikiPage);
-                    if (suiteTeardown != null) {
-                        includePage(suiteTeardown, teardown2);
-                    }
+                    includeInherited(pageName4, teardown2);
                 }
             }
 
             pageData.setContent(buffer.toString());
             return pageData.getHtml();
+        }
+
+        private void includeInherited(String pageName, String setup) {
+            WikiPage suiteSetup = PageCrawlerImpl.getInheritedPage(pageName, wikiPage);
+            if (suiteSetup != null) {
+                includePage(suiteSetup, setup);
+            }
         }
 
         private void includePage(WikiPage suiteSetup, String setup) {
